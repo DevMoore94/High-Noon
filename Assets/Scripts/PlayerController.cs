41ab37ad81a0426fac3be2ecc5aac465e23e9e37
Brandon Moore <brmo00206@gmail.com>
2017-01-16 10:05:33 -0400

    Init Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    int playerHealth;
+    Transform player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        move(h, v);
+
+	}
+
+    void move(float h, float v)
+    {
+
+    }
+}
